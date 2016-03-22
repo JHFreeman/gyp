876fe7ec946489e76e4efb9b4fb1f90d94406517
John Freeman <john@brainkernel.com>
2016-03-22 00:04:35 -0500

    Update third-party.sh

--- third-party.sh
+++ third-party.sh
@@ -19,7 +19,7 @@ rm libpng.tar.gz
 ## libjpeg-turbo ##
 wget "http://jaist.dl.sourceforge.net/project/libjpeg-turbo/1.4.0/libjpeg-turbo-1.4.0.tar.gz" -O "./libjpeg-turbo.tar.gz"
 tar xvf libjpeg-turbo.tar.gz
-mv -f libjpeg-turbo-1.4.2 ./third-party/libjpeg-turbo
+mv -f libjpeg-turbo-1.4.0 ./third-party/libjpeg-turbo
 cd ./third-party/libjpeg-turbo
 cmake . || (echo ':MUST: need cmake' && exit 1)
 cd -
